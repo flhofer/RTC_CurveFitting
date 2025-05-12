f54e86a9e618e3b4ca1f083856dece4dd42cea23
Florian Hofer <info@florianhofer.it>
2025-05-12 09:30:13 +0200

    update error.h from orchestrator

--- error.h
+++ error.h
@@ -7,11 +7,21 @@
 	#include <string.h>
 
 	#if DEBUG
-		extern FILE * dbg_out; // debug output file, defined in main
+		extern FILE * dbg_out; 		// debug output file, defined in main
+		extern FILE * stats_out; 	// debug output file, defined in main
 		#define printDbg(...) (void)fprintf (dbg_out, __VA_ARGS__)
+		#define printStat(...) (void)fprintf (stats_out, __VA_ARGS__)
 	#else
 		#define printDbg(...) //
+		#define printStat(...) //
 	#endif
+
+	// Common standard printing definitions
+	#define PFX "[orchestrator] "
+	#define PFL "         "PFX
+	#define PIN PFX"    "
+	#define PIN2 PIN"    "
+	#define PIN3 PIN2"    "
 
 	// general log information
 	void debug(char *fmt, ...);
@@ -30,9 +40,21 @@
 	// interal error print function
 	void err_doit(int err, const char *fmt, va_list ap);
 
+	// general default
+	#define KNRM  "\x1B[0m"
+	#define KRED  "\x1B[31m"
+	#define KGRN  "\x1B[32m"
+	#define KYEL  "\x1B[33m"
+	#define KBLU  "\x1B[34m"
+	#define KMAG  "\x1B[35m"
+	#define KCYN  "\x1B[36m"
+	#define KWHT  "\x1B[37m"
+
 	/* exit codes */
-	#define EXIT_SUCCESS 0
-	#define EXIT_FAILURE 1
+	#ifndef EXIT_SUCCESS
+		#define EXIT_SUCCESS 0
+		#define EXIT_FAILURE 1
+	#endif
 	#define EXIT_INV_CONFIG 2
 	#define EXIT_INV_COMMANDLINE 3
 
